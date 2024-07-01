a8c9bbd4a56842fc17d9c94834137c660ad1e851
Ali Abdkadir <ali.abdikadir@student.moringaschool.com>
2024-07-01 03:00:47 +0300

    stringslab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = 'Ali Abdikadir'
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+`Ali Abdikadir`[0];
+
